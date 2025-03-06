4796ffb1a8d4e489c5dcd1da456c76a5521b920b
CabosManuel <cabos.apolaya@gmail.com>
2025-03-06 16:30:08 -0500

    Protected and Static

---
+++ src/09-protected.ts
@@ -0,0 +1,43 @@
+export class Animal {
+  constructor(protected name: string) {}
+
+  move() {
+    console.log('Moving...');
+  }
+
+  greeting() {
+    return `Hello, I'm ${this.name}`;
+  }
+
+  protected run() {
+    console.log('Running...');
+  }
+}
+
+export class Dog extends Animal {
+  constructor(
+    name: string,
+    public owner: string
+  ) {
+    super(name);
+  }
+
+  bark(times: number) {
+    for (let index = 0; index < times; index++) {
+      console.log(`Bau! ${this.name}`);
+    }
+
+    this.run();
+  }
+
+  move() {
+    console.log('Moving like a dog...');
+    super.move();
+  }
+}
+
+const dog = new Dog('Fuwawa', 'Yago');
+// dog.name = 'Korone';
+dog.bark(1);
+dog.move();
+// dog.run();
---
+++ src/10-static.ts
@@ -0,0 +1,14 @@
+console.log("🚀 ~ Math.PI:", Math.PI);
+console.log("🚀 ~ Math.max:", Math.max(1, 2, 3, 4, 5));
+
+class MyMath {
+  static readonly PI = 3.14;
+
+  static max (...numbers: number[]) {
+    return numbers.reduce((max, item) => max >= item ? max : item);
+  }
+}
+
+console.log("🚀 ~ MyMath.PI:", MyMath.PI);
+console.log("🚀 ~ MyMath.max:", MyMath.max(1, 2, 3, 4, 5));
+console.log("🚀 ~ MyMath.max:", MyMath.max(-1, -2, -3, -4, -5));
